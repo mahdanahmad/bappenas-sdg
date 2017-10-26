20745a19944d87a031d5a15672f484f0f89b5146
Mahdan Ahmad Fauzi Al-Hasan <mahdan.hasan@un.or.id>
2017-10-26 17:26:42 +0700

    barchart with dummy

---
+++ js/barchart.js
@@ -0,0 +1,51 @@
+function createBarChart(data) {
+	let canvasWidth		= $('#barchart-container').outerWidth(true);
+	let canvasHeight	= $('#barchart-container').outerHeight(true);
+
+	let provHeight		= 20;
+
+	let margin 			= { top: 15, right: 25, bottom: 15, left: 140 };
+	let width			= canvasWidth - margin.right - margin.left;
+	let height			= provHeight * data.length;
+	// let height			= canvasHeight - margin.top - margin.bottom;
+
+	let y 				= d3.scaleBand().range([height, 0]).padding(0.1);
+	let x 				= d3.scaleLinear().range([0, width]);
+
+	let svg = d3.select("#barchart-container").append("svg")
+			    .attr("width", width + margin.left + margin.right)
+			    .attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	data.forEach((o) => { o.value = +o.value; });
+
+	x.domain([0, d3.max(data, (o) => (o.value))]);
+	y.domain(data.map((o) => (o.name)));
+
+	svg.selectAll(".bar")
+			.data(data)
+	    .enter().append("rect")
+			.attr("class", "bar")
+			.attr("width", (o) => (x(o.value)))
+			.attr("y", (o) => (y(o.name)))
+			.attr("height", y.bandwidth())
+		.on("mouseover", function(o) {
+
+			let xPosition	= d3.select(this).attr("width") / 2 + margin.left - 50;
+			let yPosition	= parseFloat(d3.select(this).attr("y") - (d3.select(this).attr("y") < 28 ? -(y.bandwidth() * 2) : y.bandwidth()));
+
+			d3.select("#barchart-tooltip")
+				.style("left", xPosition + "px")
+				.style("top", yPosition + "px")
+				.select("#barchart-tooltip-value")
+				.text(o.value);
+
+			d3.select("#barchart-tooltip").classed((d3.select(this).attr("y") < 28 ? "top" : "down"), true);
+			d3.select("#barchart-tooltip").classed("hidden", false);
+
+		})
+		.on("mouseout", () => { d3.select("#barchart-tooltip").classed("hidden", true).classed("top", false).classed("down", false); });
+
+	svg.append("g").call(d3.axisLeft(y).tickSize(0));
+}
---
+++ js/main.js
@@ -0,0 +1,50 @@
+let defaultOpt	= "<option value='' disabled selected hidden>Pilih Indikator</option>";
+let noOpt		= "<option value='' disabled>Tidak Ada data tersedia untuk tujuan ini</option>";
+
+let provs		= ["Aceh", "Bali", "Banten", "Bengkulu", "Gorontalo", "Jakarta", "Jambi", "Jawa Barat", "Jawa Tengah", "Jawa Timur", "Kalimantan Barat", "Kalimantan Selatan", "Kalimantan Tengah", "Kalimantan Timur", "Kalimantan Utara", "Kepulauan Bangka Belitung", "Kepulauan Riau", "Lampung", "Maluku", "Maluku Utara", "Nusa Tenggara Barat", "Nusa Tenggara Timur", "Papua", "Papua Barat", "Riau", "Sulawesi Barat", "Sulawesi Selatan", "Sulawesi Tengah", "Sulawesi Tenggara", "Sulawesi Utara", "Sumatera Barat", "Sumatera Selatan", "Sumatera Utara", "Yogyakarta"];
+
+$( document ).ready(function() {
+	let sdgs_list	= [
+		"Tidak ada kemiskinan",
+		"Tidak ada kelaparan",
+		"Kesehatan yang baik",
+		"Pendidikan berkualitas",
+		"Kesetaraan gender",
+		"Air bersih dan sanitasi",
+		"Energi terbarukan",
+		"Pekerjaan yang baik dan pertumbuhan ekonomi",
+		"Inovasi dan infrastruktur",
+		"Berkurangnya ketidaksetaraan",
+		"Kota dan masyarakat berkelanjutan",
+		"Pemakaian yang bertanggung jawab",
+		"Aksi iklim",
+		"Kehidupan di bawah air",
+		"Kehidupan di darat",
+		"Perdamaian dan keadilan",
+		"Kemitraan untuk tujuan",
+	];
+	let sgd_active	= _.head(sdgs_list);
+
+	$('#sdg-wrapper').html(_.map(sdgs_list, (o, i) => ("<div class='sdg-container cursor-pointer' onclick=\"sdgClicked(this, '" + o + "')\">" +
+		"<img src='/public/images/sdgs_logo/" + (i + 1) + ".png'/>" +
+		"<div class='sdg-overlay'></div>" +
+	"</div>")));
+
+	$('#indikator-selector').html(defaultOpt);
+
+	setTimeout(function() {
+		$('#chart-wrapper').height($('#root').outerHeight( true ) - $('#header').outerHeight( true ) - $('#sdg-wrapper').outerHeight( true ) - $('#indikator-wrapper').outerHeight( true ));
+
+		createBarChart(_.chain(provs).map((o) => ({ name: o, value: _.random(10) })).sortBy('value').value());
+	}, 100);
+
+});
+
+function sdgClicked(elem, name) {
+	$('.sdg-container.active').removeClass('active');
+	$(elem).addClass('active');
+	$('#selected-sdg').text(name);
+
+	let indicators	= _.chain(5).random().times((o) => ("indikator " + (o + 1))).value();
+	$('#indikator-selector').html(defaultOpt + (indicators.length > 0 ? _.map(indicators, (o) => ('<option>' + o + '</option>')) : noOpt ));
+}
